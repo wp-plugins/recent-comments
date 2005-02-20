f3c2d1f0d7c18289e021e1e540f4827692d1ca9d
MtDewVirus <MtDewVirus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-02-20 16:08:46 +0000

    Updating for WP 1.5
    git-svn-id: https://plugins.svn.wordpress.org/recent-comments/branches/wp-1.5@795 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ recent-comments.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: Recent Comments
+Plugin URI: http://mtdewvirus.com/code/
+Description: Retrieves a list of the most recent comments.
+Version: 1.15
+Author: Nick Momrik
+Author URI: http://mtdewvirus.com/
+*/
+
+function mdv_recent_comments($no_comments = 5, $comment_lenth = 5, $before = '<li>', $after = '</li>', $show_pass_post = false, $comment_style = 0) {
+    global $wpdb;
+    $request = "SELECT ID, comment_ID, comment_content, comment_author, post_title FROM $wpdb->comments LEFT JOIN $wpdb->posts ON $wpdb->posts.ID=$wpdb->comments.comment_post_ID WHERE post_status = 'publish' ";
+	if(!$show_pass_post) $request .= "AND post_password ='' ";
+	$request .= "AND comment_approved = '1' ORDER BY comment_ID DESC LIMIT $no_comments";
+	$comments = $wpdb->get_results($request);
+    $output = '';
+	if ($comments) {
+		foreach ($comments as $comment) {
+			$comment_author = stripslashes($comment->comment_author);
+			if ($comment_author == "")
+				$comment_author = "anonymous"; 
+			$comment_content = strip_tags($comment->comment_content);
+			$comment_content = stripslashes($comment_content);
+			$words=split(" ",$comment_content); 
+			$comment_excerpt = join(" ",array_slice($words,0,$comment_lenth));
+			$permalink = get_permalink($comment->ID)."#comment-".$comment->comment_ID;
+			if ($comment_style == 1) {
+				$post_title = stripslashes($comment->post_title);
+				$output .= $before . '<a href="' . $permalink . '" title="View the entire comment by ';
+				$output .= $comment_author.'">' . $comment_author . '</a> on ' . $post_title . '.' . $after;
+			}
+			else {
+				$output .= $before . '<strong>' . $comment_author . ':</strong> <a href="' . $permalink;
+				$output .= '" title="View the entire comment by ' . $comment_author.'">' . $comment_excerpt.'</a>' . $after;
+			}
+		}
+		$output = convert_smilies($output);
+	} else {
+		$output .= $before . "None found" . $after;
+	}
+    echo $output;
+}
+?>
