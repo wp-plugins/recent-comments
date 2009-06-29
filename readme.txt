db26eb67038598dc03b633f6e23e65fa1cee6975
MtDewVirus <MtDewVirus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-29 14:50:11 +0000

    organize code and add an option to hide pingbacks and trackbacks
    git-svn-id: https://plugins.svn.wordpress.org/recent-comments/trunk@130426 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,8 +1,18 @@
-=== Recent Comments ===Contributors: MtDewVirusTags: comments, recent, listStable tag: trunkRetrieves a list of the most recent comments.== Installation ==1. Upload `recent-comments.php` to the `/wp-content/plugins/` directory1. Activate the plugin through the 'Plugins' menu in WordPress1. Place `<?php mdv_recent_comments(); ?>` in your templates.
+=== Recent Comments ===
+Contributors: MtDewVirus
+Tags: comments, recent, list
+Stable tag: trunk
+
+Retrieves a list of the most recent comments.
+
+== Installation ==
+1. Upload `recent-comments.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php mdv_recent_comments(); ?>` in your templates.
 
 == Configuration ==
 You may pass parameters when calling the function to configure some of the options.
-Example: `mdv_recent_comments(10, 7, '', '<br />', true, 1)`
+Example: `mdv_recent_comments(10, 7, '', '<br />', true, 1, true)`
 
 The parameters:
 $no_comments - sets the number of recent comments to display
@@ -13,3 +23,4 @@ $show_pass_post - whether or not to display comments from password protected pos
 $comment_style - sets the style of comment to be used
 	1 = "CommenterName on PostTitle" with CommenterName being a link to the comment
 	0 = "CommenterName: WordsOfComment" with WordsOfComment being a link to the comment
+$hide_pingbacks_trackbacks - when set to true, only comments will be displayed
--- recent-comments.php
+++ recent-comments.php
@@ -3,48 +3,68 @@
 Plugin Name: Recent Comments
 Plugin URI: http://mtdewvirus.com/code/wordpress-plugins/
 Description: Retrieves a list of the most recent comments.
-Version: 1.18
+Version: 1.2
 Author: Nick Momrik
 Author URI: http://mtdewvirus.com/
 */
 
-function mdv_recent_comments($no_comments = 5, $comment_lenth = 5, $before = '<li>', $after = '</li>', $show_pass_post = false, $comment_style = 0) {
-    global $wpdb;
-    $request = "SELECT ID, comment_ID, comment_content, comment_author, comment_author_url, post_title FROM $wpdb->comments LEFT JOIN $wpdb->posts ON $wpdb->posts.ID=$wpdb->comments.comment_post_ID WHERE post_status IN ('publish','static') ";
-	if(!$show_pass_post) $request .= "AND post_password ='' ";
-	$request .= "AND comment_approved = '1' ORDER BY comment_ID DESC LIMIT $no_comments";
-	$comments = $wpdb->get_results($request);
-    $output = '';
-	if ($comments) {
-		foreach ($comments as $comment) {
-			$comment_author = stripslashes($comment->comment_author);
-			if ($comment_author == "")
-				$comment_author = "anonymous"; 
-			$comment_content = strip_tags($comment->comment_content);
-			$comment_content = stripslashes($comment_content);
-			$words=split(" ",$comment_content); 
-			$comment_excerpt = join(" ",array_slice($words,0,$comment_lenth));
-			$permalink = get_permalink($comment->ID)."#comment-".$comment->comment_ID;
+function most_recent_comments($no_comments = 5, $comment_lenth = 5, $before = '<li>', $after = '</li>', $show_pass_post = false, $comment_style = 0, $hide_pingbacks_trackbacks = false) {
+	global $wpdb;
+	$most_recent_comments = wp_cache_get('most_recent_comments');
+	
+	if ( false === $most_recent_comments ) {
+		$request = "SELECT ID, comment_ID, comment_content, comment_author, comment_author_url, post_title FROM $wpdb->comments LEFT JOIN $wpdb->posts ON $wpdb->posts.ID=$wpdb->comments.comment_post_ID WHERE post_status IN ('publish','static') ";
+		if( !$show_pass_post ) $request .= "AND post_password ='' ";
+		if ( $hide_pingbacks_trackbacks ) $request .= "AND comment_type='' ";
+		$request .= "AND comment_approved = '1' ORDER BY comment_ID DESC LIMIT $no_comments";
 
-			if ($comment_style == 1) {
-				$post_title = stripslashes($comment->post_title);
+		$comments = $wpdb->get_results($request);
+	
+		$output = '';
+
+		if ( $comments ) {
+			$idx = 0;
+			foreach ($comments as $comment) {
+				$comment_author = stripslashes($comment->comment_author);
+				if ($comment_author == "")
+					$comment_author = "anonymous"; 
+				$comment_content = strip_tags($comment->comment_content);
+				$comment_content = stripslashes($comment_content);
+				$words = split(" ", $comment_content); 
+				$comment_excerpt = join(" ", array_slice($words, 0, $comment_lenth));
+				$permalink = get_permalink($comment->ID) . "#comment-" . $comment->comment_ID;
 				
-				$url = $comment->comment_author_url;
+				if ( 1 == $comment_style ) {
+					$post_title = stripslashes($comment->post_title);
+					$post_id= stripslashes($comment->post_id);
+					$url = $comment->comment_author_url;
+					$idx++;
+					if ( 1 == $idx % 2 )
+						$before = "<li class='statsclass1'>";
+					else
+						$before = "<li class='statsclass2'>";
+					$output .= $before . "<a href='$permalink'>$comment_author</a>" . ' on <a href="' . get_permalink($comment->ID) . '">' . $post_title . '</a>' . $after;
+				} else {
+					$idx++;
+					if ( 1 == $idx % 2 )
+						$before = "<li class='statsclass1'>";
+					else
+						$before = "<li class='statsclass2'>";
+		
+					$output .= $before . '<strong>' . $comment_author . ':</strong> <a href="' . $permalink;
+					$output .= '" title="View the entire comment by ' . $comment_author.'">' . $comment_excerpt.'</a>' . $after;
+				}
+			}
 
-				if (empty($url))
-					$output .= $before . $comment_author . ' on ' . $post_title . '.' . $after;
-				else
-					$output .= $before . "<a href='$url' rel='external'>$comment_author</a>" . ' on ' . $post_title . '.' . $after;
-			}
-			else {
-				$output .= $before . '<strong>' . $comment_author . ':</strong> <a href="' . $permalink;
-				$output .= '" title="View the entire comment by ' . $comment_author.'">' . $comment_excerpt.'</a>' . $after;
-			}
+			$output = convert_smilies($output);
+		} else {
+			$output .= $before . "None found" . $after;
 		}
-		$output = convert_smilies($output);
-	} else {
-		$output .= $before . "None found" . $after;
+
+		$most_recent_comments = $output;
+		wp_cache_set('most_recent_comments', $most_recent_comments);
 	}
-    echo $output;
+
+	echo $most_recent_comments;
 }
 ?>
